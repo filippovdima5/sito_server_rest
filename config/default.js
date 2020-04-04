c2f93c57d1de132959682f3061a912cfcff1a5c2
filippovdima5 <filippovdima5yandex.ru>
2020-04-04 22:33:41 +0300

    new mongo uri

--- config/default.js
+++ config/default.js
@@ -3,17 +3,11 @@ module.exports = {
     port: 8080,
   },
 
-
     mongodb: {
-        uri: 'mongodb://localhost:27017/sito_v-2',
+        uri: 'mongodb://localhost:27017/sito-rest',
         debug: true,
     },
 
-
-  logger: {
-    env: 'sito_app_server', // информация о проекте в логах
-    vk: 'd92e60b5b766258920140c7dc4db7d7528d2ad22cb8f7c5ab8c77f6f92ba37073d370bdae44372886acef' //test-sito
-  },
 
   crypto: {
     hash: {
--- config/production.js
+++ config/production.js
@@ -4,13 +4,8 @@ module.exports = {
     },
 
     mongodb: {
-        uri: 'mongodb://mongo-root:24D1ma055x@84.201.185.241:27017',
+        uri: 'mongodb://mongo-root:24D1ma055x@84.201.185.241:27017/sito-rest?authSource=admin',
         debug: false,
-    },
-
-    logger: {
-        env: 'sito_app_server', // информация о проекте в логах
-        vk: 'd92e60b5b766258920140c7dc4db7d7528d2ad22cb8f7c5ab8c77f6f92ba37073d370bdae44372886acef' //test-sito
     },
 
     crypto: {
